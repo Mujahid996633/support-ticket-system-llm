d74b2c93ac20c430ac64ac06871f0d9745e1213b
Mujahid996633 <mujju996633@gmail.com>
2026-02-18 15:48:16 +0530

    feat: connect tickets app to main project routing.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tickets.urls')), # This connects our app URLs to the /api/ prefix
+    path('api/', include('tickets.urls')),
 ]
